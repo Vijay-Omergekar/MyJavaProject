004988ad6b6bc754329256b50aff5876cbf61999
Vijay Omergekar <omergekar.vijay@gmail.com>
2023-12-24 21:42:30 +0530

    First Git hub project with Java code

---
+++ demo/src/test/java/com/picsanddabs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picsanddabs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
